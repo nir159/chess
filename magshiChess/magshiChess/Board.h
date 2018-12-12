02ee76746d16858f6690b538ba4666f34ac5f6ba
Daniel Zigler <daniel.zsh@gmail.com>
2018-12-12 22:04:59 +0200

    Added Template for Classes: Board

---
+++ magshiChess/magshiChess/Board.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Piece.h"
+
+#define SIDE_OF_BOARD 8
+#define PIECES_PER_PLAYER 16
+
+class Board
+{
+private:
+	Piece* Pieces[SIDE_OF_BOARD][SIDE_OF_BOARD];
+public:
+	Board();
+	~Board();
+};
+

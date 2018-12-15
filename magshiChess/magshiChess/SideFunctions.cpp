b0e9997e6917def0b75163589644a511ccaf21ee
Daniel Zigler <daniel.zsh@gmail.com>
2018-12-15 20:56:57 +0200

    Added Comments for SideFunctions Class's isCharInstructionValid, isNumInstructionValid functions

--- magshiChess/magshiChess/SideFunctions.cpp
+++ magshiChess/magshiChess/SideFunctions.cpp
@@ -1,10 +1,24 @@
 #include "SideFunctions.h"
 
+/*
+function will check if character in Instuction is valid if it is return true, else return false
+Input:
+	ch - character to check
+Output:
+	if character in Instuction is valid return true, else return false
+*/
 bool SideFunctions::isCharInstructionValid(const char ch) {
 	if (ch <= STARTOF_WIDTH && ch >= ENDOF_WIDTH) { return true; }
 	return false;
 }
 
+/*
+function will check if number in Instuction is valid if it is return true, else return false
+Input:
+	num - number to check
+Output:
+	if number in Instuction is valid return true, else return false
+*/
 bool SideFunctions::isNumInstructionValid(const int num) {
 	if (num <= STARTOF_LENGTH && num >= ENDOF_LENGTH) { return true; }
 	return false;

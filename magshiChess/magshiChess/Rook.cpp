9f0b8c65cbc0421547d5900e9a3f47b69b5c2cb4
Daniel Zigler <daniel.zsh@gmail.com>
2018-12-16 18:54:09 +0200

    Added Comments for Rook::skippedPlayers function

--- magshiChess/magshiChess/Rook.cpp
+++ magshiChess/magshiChess/Rook.cpp
@@ -27,39 +27,43 @@ bool Rook::moveFormat(std::string str) {
 	return true;
 }
 
+/*
+function will check if the piece will skip any players by making the move
+Input:
+	str - Instruction to check
+	board - game board
+Output:
+	if the piece will skip any players by making the move then return true, else return false
+*/
 bool skippedPlayers(std::string str, Piece* board[][ENDOF_LENGTH]) {
 	bool rightSide = false;
 	int minRange, MaxRange, i = 0;
 
-	if (str[FIRST] > str[THIRD]) {
+	if (str[FIRST] > str[THIRD]) { // checks if 1st Part's Char is Bigger then 2nd Part's Char
+		/* Sets Fitting Values for Range & Side in for loop */
 		minRange = str[THIRD] - STARTOF_TYPE_P1;
 		MaxRange = str[FIRST] - STARTOF_TYPE_P1;
 	}
-	else if (str[FIRST] < str[THIRD]) {
+	else if (str[FIRST] < str[THIRD]) { // checks if 2nd Part's Char is Bigger then 1st Part's Char
+		/* Sets Fitting Values for Range & Side in for loop */
 		MaxRange = str[THIRD] - STARTOF_TYPE_P1;
 		minRange = str[FIRST] - STARTOF_TYPE_P1;
 	}
-	else if (str[SECOND] > str[FORTH]) {
+	else if (str[SECOND] > str[FORTH]) { // checks if 1st Part's Num is Bigger then 2nd Part's Num
+		/* Sets Fitting Values for Range & Side in for loop */
 		rightSide = true;
 		MaxRange = str[SECOND] - STARTOF_TYPE_P1;
 		minRange = str[FORTH] - STARTOF_TYPE_P1;
 	}
-	else if (str[SECOND] < str[FORTH]) {
+	else if (str[SECOND] < str[FORTH]) { // checks if 2nd Part's Num is Bigger then 1st Part's Num
+		/* Sets Fitting Values for Range & Side in for loop */
 		rightSide = true;
 		minRange = str[SECOND] - STARTOF_TYPE_P1;
 		MaxRange = str[FORTH] - STARTOF_TYPE_P1;
 	}
-	for (i = minRange + 1; i < MaxRange; i++) {
-		if (rightSide) {
-			if (board[(str[FIRST] - STARTOF_TYPE_P1)][i]->getType() != BLANK) {
-				return true;
-			}
-		}
-		else {
-			if (board[(char)(i + STARTOF_TYPE_P1)][str[SECOND]]->getType() != BLANK) {
-				return true;
-			}
-		}
+	for (i = minRange + 1; i < MaxRange; i++) { // runs on requested Range & Side in game board
+		if (rightSide && board[(str[FIRST] - STARTOF_TYPE_P1)][i]->getType() != BLANK) { /* checks if Side is Length (Right Side) && if it is check if at Length Piece skips any Other Pieces */ return true; }		
+		else if (board[(char)(i + STARTOF_TYPE_P1)][str[SECOND]]->getType() != BLANK) { /* else if it is check if at Width Piece skips any Other Pieces */ return true; }	
 	}
 	return false;
 }

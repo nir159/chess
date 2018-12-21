1fd009ad01e64c17b6758ddd080e13bd86f5aec9
Daniel Zigler <daniel.zsh@gmail.com>
2018-12-21 12:59:41 +0200

    Added Pawn::hasSkippedPlayers function

--- magshiChess/magshiChess/Pawn.cpp
+++ magshiChess/magshiChess/Pawn.cpp
@@ -25,3 +25,15 @@ bool Pawn::moveFormat(std::string instruction)
 	if (instruction[FORTH] - instruction[SECOND] == ONE && instruction[FIRST] == instruction[THIRD]) { return true; }
 	return false;
 }
+
+/*
+function will check if the piece will skip any players by making the move
+Input:
+	instruction - Instruction to check
+	gameBoard - game board
+Output:
+	if the piece will skip any players by making the move then return true, else return false
+*/
+bool Pawn::hasSkippedPlayers(std::string instruction, Piece* const gameBoard[ENDOF_LENGTH][ENDOF_LENGTH]) {
+	return false; // pawn can't skip players moves only one tile
+}
--- magshiChess/magshiChess/Pawn.h
+++ magshiChess/magshiChess/Pawn.h
@@ -6,6 +6,7 @@ class Pawn :
 public:
 	Pawn(char type);
 	bool moveFormat(std::string instruction);
+	bool hasSkippedPlayers(std::string instruction, Piece* const gameBoard[ENDOF_LENGTH][ENDOF_LENGTH]);
 	~Pawn();
 };
 
